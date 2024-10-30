8ea14307c84d896880785cbabc8abbc5d99838d6
taikantomita <63420525+taikantomita@users.noreply.github.com>
2024-10-29 23:20:00 -0400

    test python linter with good code

---
+++ backend/good-python.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+def my_function():
+    a = 5 + 3  # Proper spacing around operators
+    if a > 10:
+        print("Value is greater than 10")  # Indentation and line length are correct
+
+my_function()
