b38b87d99e0d870f20a132fc0dd1c3905e6bb7df
taikantomita <63420525+taikantomita@users.noreply.github.com>
2024-10-29 23:18:28 -0400

    test python linter with badly formatted code

---
+++ backend/test-lint.py
@@ -0,0 +1,8 @@
+import os, sys  # Multiple imports on one line, unused imports
+
+def my_function():
+    a=5+3   # Missing whitespace around operator
+    if a > 10:
+       print("Value is greater than 10") # Indentation issue and line too long
+
+my_function()
