745c09766228980f1a894cee4ffde2a8981058c5
taikantomita <taikantomita@gmail.com>
2024-10-29 23:36:02 -0400

    add autopep8

--- backend/good-python.py
+++ backend/good-python.py
@@ -1,9 +1,8 @@
-import os
-import sys
-
 def my_function():
     a = 5 + 3  # Proper spacing around operators
     if a > 10:
-        print("Value is greater than 10")  # Indentation and line length are correct
+        # Indentation and line length are correct
+        print("Value is greater than 10")
+
 
 my_function()
