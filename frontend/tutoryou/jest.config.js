4cb7cc1d4c91246df1d5b5406d03ab1275feab3b
taikantomita <taikantomita@gmail.com>
2024-11-08 02:08:01 -0500

    switch transpiler from babel to swc

--- frontend/tutoryou/jest.config.js
+++ frontend/tutoryou/jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest', // If you're using ts-jest for type-checking, you can keep this.
   setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js'],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx|js|jsx)$': ['@swc/jest'],
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy', // Handles CSS imports
   },
 }
